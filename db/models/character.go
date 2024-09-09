8d96764b7c27c85b026f7e4828e2c90faf9800cd
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-09 11:55:40 +0100

    Added a profile picture url field to the character model

--- db/models/character.go
+++ db/models/character.go
@@ -1,11 +1,12 @@
 package models
 
 type Character struct {
-	ID      int    `gorm:"autoIncrement;primary_key" json:"id"`
-	Name    string `gorm:"not null" json:"name"`
-	Level   int    `gorm:"not null" json:"level"`
-	ClassID int    `json:"class_id"`
-	RaceID  int    `json:"race_id"`
+	ID                int    `gorm:"autoIncrement;primary_key" json:"id"`
+	Name              string `gorm:"not null" json:"name"`
+	Level             int    `gorm:"not null" json:"level"`
+	ProfilePictureURL string `json:"profile_picture_url"`
+	ClassID           int    `json:"class_id"`
+	RaceID            int    `json:"race_id"`
 
 	Class *Class `json:"class" gorm:"foreignKey:ClassID;references:ID"`
 	Race  *Race  `json:"race" gorm:"foreignKey:RaceID;references:ID"`
